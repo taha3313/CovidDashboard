e7ff657ccf1bddd406fd527e6e3045fc71df7838
taha3313 <chellytaha@gmail.com>
2025-02-17 18:06:56 -0500

    Updates

---
+++ precache-manifest.02b200c1e27f5ad3b8a9c527220b1519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b5479d6d7723c11f0841054030acd1",
+    "url": "/CovidDashboard/index.html"
+  },
+  {
+    "revision": "d8def5182ac84b974b57",
+    "url": "/CovidDashboard/static/css/main.04505143.chunk.css"
+  },
+  {
+    "revision": "c137ca8c2b88d18ce047",
+    "url": "/CovidDashboard/static/js/2.17bd7f60.chunk.js"
+  },
+  {
+    "revision": "9c0c47d34de10fa07a7c",
+    "url": "/CovidDashboard/static/js/3.cf3acbb1.chunk.js"
+  },
+  {
+    "revision": "d8def5182ac84b974b57",
+    "url": "/CovidDashboard/static/js/main.449014b5.chunk.js"
+  },
+  {
+    "revision": "29a5bae8b7a2e5f3726e",
+    "url": "/CovidDashboard/static/js/runtime~main.778c895c.js"
+  }
+]);
