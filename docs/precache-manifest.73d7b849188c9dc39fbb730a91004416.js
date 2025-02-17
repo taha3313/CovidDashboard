82190a19691f2009b7b066f00422ebe0ddbd2742
taha3313 <chellytaha@gmail.com>
2025-02-17 17:57:29 -0500

    Deploy to GitHub Pages

---
+++ docs/precache-manifest.73d7b849188c9dc39fbb730a91004416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba13b17246359fc681a7e6db1c25102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8def5182ac84b974b57",
+    "url": "/static/css/main.04505143.chunk.css"
+  },
+  {
+    "revision": "c137ca8c2b88d18ce047",
+    "url": "/static/js/2.17bd7f60.chunk.js"
+  },
+  {
+    "revision": "9c0c47d34de10fa07a7c",
+    "url": "/static/js/3.cf3acbb1.chunk.js"
+  },
+  {
+    "revision": "d8def5182ac84b974b57",
+    "url": "/static/js/main.449014b5.chunk.js"
+  },
+  {
+    "revision": "3ad64bfc4cfd8e8d8f04",
+    "url": "/static/js/runtime~main.3bdd5ab3.js"
+  }
+]);
